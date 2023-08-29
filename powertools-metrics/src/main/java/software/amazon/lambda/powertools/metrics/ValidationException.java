3a2a1092fbde347912725d5867a8c69fe2497432
Simon Thulbourn <sthulb@users.noreply.github.com>
2023-08-29 10:35:41 +0000

    do the thing

--- powertools-metrics/src/main/java/software/amazon/lambda/powertools/metrics/ValidationException.java
+++
@@ -1,8 +0,0 @@
-package software.amazon.lambda.powertools.metrics;
-
-public class ValidationException extends RuntimeException {
-
-    public ValidationException(String message) {
-        super(message);
-    }
-}
