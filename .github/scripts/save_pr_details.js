65a760fd612cbc6af9fdcce96bdb805940f722b5
Simon Thulbourn <sthulb@users.noreply.github.com>
2023-09-07 22:18:01 +0200

    Update save_pr_details.js

--- .github/scripts/save_pr_details.js
+++ .github/scripts/save_pr_details.js
@@ -1,7 +1,15 @@
-module.exports = async ({context, core}) => {
+module.exports = async ({github, context, core}) => {
     const fs = require('fs');
     const filename = "pr.txt";
 
+    const labelsData = await github.rest.issues.listLabelsOnIssue(
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        issue_number: (context.payload.issue || context.payload.pull_request || context.payload).number,
+    );
+
+    console.log(labelsData)
+    
     try {
         fs.writeFileSync(`./${filename}`, JSON.stringify(context.payload));
 
