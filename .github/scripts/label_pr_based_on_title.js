d5f5aea7c08c1ac58b312e952e0389be7508d142
Simon Thulbourn <sthulb@users.noreply.github.com>
2023-09-07 23:04:26 +0200

    Update label_pr_based_on_title.js

--- .github/scripts/label_pr_based_on_title.js
+++ .github/scripts/label_pr_based_on_title.js
@@ -33,7 +33,7 @@ module.exports = async ({github, context, core}) => {
             if (matches != null) {
                 core.info(`Auto-labeling PR ${PR_NUMBER} with ${label}`)
 
-                for (const prLabel in prLabels) {
+                prLabels.forEach((i, prLabel) => {
                     core.info(`evaluating label: ${prLabel}`)
                     if (labelKeys.includes(prLabel)) {
                         core.info(`${prLabel} is a reserved label, we should remove it.`);
@@ -44,7 +44,7 @@ module.exports = async ({github, context, core}) => {
                             name: prLabel
                         })
                     }
-                }
+                })
                 
                 await github.rest.issues.addLabels({
                     issue_number: PR_NUMBER,
